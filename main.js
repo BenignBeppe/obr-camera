a795e10c1ba3e72eda272e3e5e4aea5665f13221
Sebastian Berlin <berlin.sebastian@gmail.com>
2023-09-10 19:35:10 +0200

    Save and load camera parameters
    Position and scale of the viewport is loaded when the extension is
    initialised. The same parameters are saved once per second.

--- main.js
+++ main.js
@@ -14,6 +14,27 @@ function getPluginId(path) {
 
 function init() {
     log("Starting");
+
+    loadCamera();
+    setInterval(saveCamera, 1000);
+}
+
+function loadCamera() {
+    let camera = JSON.parse(localStorage.getItem(getPluginId()));
+    if(!camera) {
+        return;
+    }
+
+    OBR.viewport.setPosition(camera.position);
+    OBR.viewport.setScale(camera.scale);
+}
+
+async function saveCamera() {
+    let camera = {
+        position: await OBR.viewport.getPosition(),
+        scale: await OBR.viewport.getScale()
+    };
+    localStorage.setItem(getPluginId(), JSON.stringify(camera));
 }
 
 OBR.onReady(async () => {
