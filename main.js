f968b7198c9505fffcea4f89f97b8aeb55557580
Sebastian Berlin <berlin.sebastian@gmail.com>
2023-12-27 10:21:44 +0100

    Save and load the centre of the viewport
    This makes loading focus on the same point regardless of window size.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import OBR from "@owlbear-rodeo/sdk";
 
+// One minute between saves.
+let cameraSaveInterval = 1000 * 60;
+
 function log(...message) {
     console.log(`${getPluginId()}:`, ...message);
 }
@@ -16,22 +19,30 @@ function init() {
     log("Starting");
 
     loadCamera();
-    setInterval(saveCamera, 1000);
+    setInterval(saveCamera, cameraSaveInterval);
 }
 
-function loadCamera() {
+async function loadCamera() {
     let camera = JSON.parse(localStorage.getItem(getPluginId()));
     if(!camera) {
         return;
     }
 
-    OBR.viewport.setPosition(camera.position);
+    let focus = camera.position;
+    // Offset camera to load the centre point rather than top left.
+    focus.x += await OBR.viewport.getWidth() / 2;
+    focus.y += await OBR.viewport.getHeight() / 2;
+    OBR.viewport.setPosition(focus);
     OBR.viewport.setScale(camera.scale);
 }
 
 async function saveCamera() {
+    let focus = await OBR.viewport.getPosition();
+    // Offset camera to save the centre point rather than top left.
+    focus.x -= await OBR.viewport.getWidth() / 2;
+    focus.y -= await OBR.viewport.getHeight() / 2;
     let camera = {
-        position: await OBR.viewport.getPosition(),
+        position: focus,
         scale: await OBR.viewport.getScale()
     };
     localStorage.setItem(getPluginId(), JSON.stringify(camera));
